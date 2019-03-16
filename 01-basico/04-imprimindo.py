991a751552f014532604c4c63a238e693fdc9e25
jaimersoncorreia <jaimerson_correia@hotmail.com>
2019-03-16 19:22:52 -0400

    Imprimindo nossos dados na tela

---
+++ 01-basico/04-imprimindo.py
@@ -0,0 +1,9 @@
+a = 42
+print('O número é ', a)
+print('O número', a, 'é muito legal')
+print('O número %d é muito legal' %a)#mardadores
+b = 'abacate'
+print('Uma fruta muito gostosa é o %s. Na opinião da massa!' %b)
+c = 3.1415
+print('Pi é %f' %c)
+print('Pi com duas casas %.2f' %c)
