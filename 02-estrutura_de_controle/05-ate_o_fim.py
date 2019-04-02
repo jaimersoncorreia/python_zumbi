5859b4a57ea73472d396fa6470fb4ebf232f4c15
jaimersoncorreia <jaimerson_correia@hotmail.com>
2019-04-02 16:52:02 -0400

    Repetições e Inteiros até um fim

---
+++ 02-estrutura_de_controle/05-ate_o_fim.py
@@ -0,0 +1,13 @@
+num = int(input("Forneça um número: "))
+x = 1
+while x <= num:
+	if x % 2 == 0:
+		print(x)
+	x += 1
+
+exit()
+fim = int(input("Digite o último número: "))
+x = 1
+while x <= fim:
+	print(x)
+	x += 1
